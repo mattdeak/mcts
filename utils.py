69356ed4f3b971b0cf7602585af4e72b79d48582
Matthew Deakos <mwoolnerdeakos@gmail.com>
2018-03-09 18:28:00 -0500

    Fixed error in UCB1 formula

--- utils.py
+++ utils.py
@@ -3,9 +3,9 @@ import numpy as np
 def ucb1_selection(node, C=1.41):
     """Returns an action that maximizes the UCB1 formula"""
     edges = node.edges
-    visit_count = sum([edge.visit_count for edge in edges.values()])
+    log_visit_count = np.log(sum([edge.visit_count for edge in edges.values()]))
 
-    ucb1_values = [[action, edge.value + C*np.sqrt(visit_count/edge.visit_count)] for action, edge in edges.items()]
+    ucb1_values = [[action, edge.value + C*np.sqrt(log_visit_count/edge.visit_count)] for action, edge in edges.items()]
     action, value = max(ucb1_values, key=lambda x: x[1])
     return action
 
