b4180d73d3c01951815591ed6a4de28f2622229d
Matthew Deakos <mwoolnerdeakos@gmail.com>
2018-03-08 19:38:00 -0500

    Fixed some errors in UCB1

--- utils.py
+++ utils.py
@@ -1,7 +1,14 @@
 import numpy as np
 
-def ucb1():
-    """Returns the UCB1 upper confidence bound"""
+def ucb1_selection(node, C=1.41):
+    """Returns an action that maximizes the UCB1 formula"""
+    edges = node.edges
+    visit_count = sum([edge.visit_count for edge in edges])
+
+    ucb1_values = [[action, edge.value + C*np.sqrt(visit_count/edge.visit_count)] for action, edge in edges.items()]
+    action, value = max(ucb1_values, lambda x: x[1])
+    return action
+
 
 def random_rollout(actions):
     return np.random.choice(actions)
