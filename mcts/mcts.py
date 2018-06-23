cf97c3d22241b9eb0ec01a1eedc215afa5cb84c3
Matt <mwoolnerdeakos@gmail.com>
2018-06-23 18:07:15 -0400

    Started integration of gametree into MCTS

--- mcts/mcts.py
+++ mcts/mcts.py
@@ -4,54 +4,39 @@ import numpy as np
 import datetime
 from sortedcontainers.sorteddict import SortedDict
 from copy import deepcopy
-
-
-class MCTNode:
-    """Node class for MCTS. Tracks state and win information."""
-    def __init__(self, state_id, state, player_turn):
-        self.id = state_id # Used to identify state
-        self.state = state
-        self.player = player_turn
-        self.n = 0
-        self.w = 0
-        self.children = SortedDict()
-
-    def add_child(self, action, child):
-        self.children[action] == child
-
-class NodeTree:
-    def __init__(self):
-        
-    def add_node(self, parent_id, action, node):
-        """Adds a node to the node tree"""
-        
-
-    def delete_node(self, node_id):
-        """Deletes a node from the node tree"""
-
-    def get_by_id(self, node_id):
-        """Retrieves a node by the node ID"""
-
-    def get_by_node(self, node):
-        """Retrieves the node"""
-
-    def reset(self):
-        self.nodes = {}
+from tree.gametree import GameTree
 
 
 
 class MCTS:
-    def __init__(self, environment, action_policy, selection_policy, simulation_policy,
-                update_policy):
+    def __init__(self, environment, action_policy, selection_policy, rollout_policy,
+                update_policy, calculation_time):
+        self.tree = GameTree()
+       
         self.environment = environment
-        self.action_policy = action_policy
-        self.selection_policy = selection_policy
+        
+        
+        self.choose = action_policy
+        self.select = selection_policy
+        self.rollout = rollout_policy
         self.simulation_policy = simulation_policy
         self.update_policy = update_policy
 
+        self.calculation_time = calculation_time
+        
+
+    @property
+    def calculation_time(self):
+        return self._calculation_time
+
+    @calculation_time.setter
+    def calculation_time(self, seconds):
+        self._calculation_time = datetime.timedelta(seconds=seconds)
+
+    def reset():
         self.terminal = False
         self.game_history = []
-
+        self.tree.reset()
     
     def act(self):
         state = self.environment.state
@@ -68,16 +53,17 @@ class MCTS:
 
         # Selection Phase
         while not done and not current.is_leaf():
-            action = self.selection_policy.act(current, env_clone)
+            action = self.selection_policy(current, env_clone)
             self.history.append(current.id)
 
         # Expansion Phase
         if not done:
-            action = self.expansion_policy.act(current, env_clone)
+            self.tree.expand(env_clone.state, env_clone.valid_actions)
+            action = self.rollout_policy(current, env_clone)
 
         # Simulation Phase
         if not done:
-            current, reward, done = self.simulation_policy.act(current, clone_env)
+            current, reward, done = self.simulation_policy(current, env_clone)
 
         # Update Phase
-        self.update_policy.update(current, env_clone, winner, reward, history)
+        self.update_policy(current, env_clone, winner, reward, history)
