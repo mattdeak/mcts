1ad3eb17b0e6c1a74a143a015f693658c9b9789f
Matt <mwoolnerdeakos@gmail.com>
2018-06-24 04:57:47 -0400

    Implemented RandomChoice and RandomUnvisited rollout policies

--- mcts/policies/rollout.py
+++ mcts/policies/rollout.py
@@ -1,9 +1,23 @@
 import numpy as np
 
-class RandomUnvisited:
+class RandomChoice:
 
     def __init__(self):
         pass
 
     def __call__(self, node, environment):
         return np.random.choice(environment.valid_actions)
+
+class RandomUnvisited:
+
+    def __call__(self, node, environment):
+        if node.expanded:
+            unvisited_actions = [a for a in node.edges if a.n == 0]
+            return np.random.choice(
+                np.intersect(
+                    actions, environment.valid_actions
+                    )
+                )
+        # If the node is a leaf node
+        else:
+            return environment.valid_actions
