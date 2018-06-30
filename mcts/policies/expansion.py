46ee8364cac826338e39fff35b8d4ece6a638e27
Matt <mwoolnerdeakos@gmail.com>
2018-06-30 06:59:48 -0400

    Added a neural-network based expansion policy

--- mcts/policies/expansion.py
+++ mcts/policies/expansion.py
@@ -1,15 +1,29 @@
-from ..base.policy import BasePolicy
+from ..base.policy import NodeTrackingPolicy
 import xxhash
 
-class VanillaExpansion(BasePolicy):
+class VanillaExpansion(NodeTrackingPolicy):
     """Expands the leaf node by adding possible actions
     as edges to the node."""
-    def add_tree(self, tree):
-        self.tree = tree
-
     def __call__(self, node, actions):
 
         node.expanded = True
         node.set_edges(actions)
+
+class NNExpansion(NodeTrackingPolicy):
+    """Expands a node using priors based on a neural net.
+    
+    As of now, only neural nets with both a value-output and a policy-output will be supported."""
+    def __init__(self, model):
+        self.model = model
+
+    def __call__(self, node, actions):
+        policy, value = self.model.predict(node.state)
+
+        # We only care about the priors for
+        # valid actions from the current state
+        valid_policies = policy[actions]
+
+        node.set_edges(actions, priors)
+        node.set_value(value)
         
         
