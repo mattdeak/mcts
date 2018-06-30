baffce0d2726f51b0de93385b67ba56b6ffc74fe
Matt <mwoolnerdeakos@gmail.com>
2018-06-30 06:44:27 -0400

    Fixed bug causing improper handling of drawed games

--- mcts/policies/update.py
+++ mcts/policies/update.py
@@ -2,6 +2,7 @@ from ..base.policy import BasePolicy
 import numpy as np
 
 class Vanilla(BasePolicy):
+    """The update policy that follows vanilla MCTS practices."""
 
     def add_tree(self, tree):
         self.tree = tree
@@ -14,14 +15,16 @@ class Vanilla(BasePolicy):
         winner = environment.winner
         self._logger.debug(f"Updating winner: {winner}")
 
+        # No winner means a draw-state was reached
         # TODO: Handle for non 2-player games
         if winner == None:
-            winner = np.random.randint(environment.n_players)
-            reward = np.random.randint(2)
+            # Choose a random winner. This will average out to 
+            # value of 0.5 for draw-states
+            winner = np.random.randint(environment.n_players) + 1
 
         for node_id, action in history:
 
             node = self.tree.get_by_id(node_id)
             node[action].n += 1
-            if node.player == environment.winner:
+            if node.player == winner:
                 node[action].w += 1
