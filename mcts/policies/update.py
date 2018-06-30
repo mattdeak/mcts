43693a846f4c03d603e7b7db84faec007c40dfb2
Matt <mwoolnerdeakos@gmail.com>
2018-06-30 11:09:12 -0400

    Added value-based update policy

--- mcts/policies/update.py
+++ mcts/policies/update.py
@@ -1,19 +1,14 @@
-from ..base.policy import BasePolicy
+from ..base.policy import NodeTrackingPolicy
 import numpy as np
 
-class Vanilla(BasePolicy):
+class Vanilla(NodeTrackingPolicy):
     """The update policy that follows vanilla MCTS practices."""
-
-    def add_tree(self, tree):
-        self.tree = tree
-
     def __call__(self, environment, reward, history):
         """Updates the nodes in the node tree.
         
         Increments visit count and win count if the node is winning"""
         
         winner = environment.winner
-        self._logger.debug(f"Updating winner: {winner}")
 
         # No winner means a draw-state was reached
         # TODO: Handle for non 2-player games
@@ -28,3 +23,31 @@ class Vanilla(BasePolicy):
             node[action].n += 1
             if node.player == winner:
                 node[action].w += 1
+
+class ValueUpdate(NodeTrackingPolicy):
+    """The update policy for an MCTS where nodes store their value based on a rollout policy or model."""
+    def __call__(self, environment, reward, history):
+        
+        node = self.tree.get_by_state(environment.state)
+
+        # If the environment is terminal then the value is the reward
+        if environment.terminal:
+            value = reward
+            node.value = reward
+
+        for node_id, action in history:
+            node = self.tree.get_by_id(node_id)
+            node[action].n += 1
+            if node.player == winner:
+                node[action].w += value
+            else:
+                node[action].w -= value
+
+        
+
+       
+        
+        
+        
+        
+        
