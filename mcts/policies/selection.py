1377e733a52c9a779404c07e37d9e68182d43ebf
Matt <mwoolnerdeakos@gmail.com>
2018-06-30 11:10:24 -0400

    Removed selection policy reliance on environment

--- mcts/policies/selection.py
+++ mcts/policies/selection.py
@@ -3,38 +3,36 @@ import numpy as np
 
 class UCB1(BasePolicy):
     """UCB1 Policy Class. Works in the selection phase to balance
-    exploration and exploitation."""
+    exploration and exploitation for Vanilla MCTS."""
     def __init__(self, C=1.41):
         self.C = C
         super().__init__()
 
-    def __call__(self, node, environment):
+    def __call__(self, node):
+        actions = node.edges.keys()
 
-        actions = environment.actions
-        epsilon = 1e-5
-
-        log_n = np.log(sum([node[action].n + epsilon for action in actions]))
-        ucb1_values = [[action, node[action].q + self.C*np.sqrt(log_n/(node[action].n + epsilon))] for action in actions]
+        log_n = np.log(sum([node[action].n for action in actions]))
+        ucb1_values = [[action, node[action].q + self.C*np.sqrt(log_n/(node[action].n + 1))] for action in actions]
         action, value = max(ucb1_values, key=lambda x: x[1])
         return action
 
 
 class PUCT(BasePolicy):
+    """UCB1 Policy Class. Chooses an action probabalistically
+    based on the priors and action-values of edges in a node."""
     def __init__(self, C=1.41):
         self.C = C
         super().__init__()
 
-    def __call__(self, node, environment):
+    def __call__(self, node):
         edges = node.edges
-        priors = [action.p for action in children.values()]
 
-        root_N = np.sqrt(np.sum([edge.n for edge in children.values()]))
-        qus = np.zeros(len(priors))
-        for i in range(len(priors)):
-            prior = priors[i]
-            edge = node[i]
+        root_N = np.sqrt(np.sum([edge.n for edge in edges.values()]))
+        qus = np.zeros(len(edges))
 
-            qu = edge.value + C * prior * root_N / (edge.n + 1e-5)
-            qus[i] = qu
+        for action, edge in edges.items():
+
+            qu = edge.q + C * edge.p * root_N / (edge.n + 1)
+            qus[action] = qu
 
         return np.argmax(qus)
