a51be43005fda3f9917aea29e0cb5a3cea7957dd
Matt <mwoolnerdeakos@gmail.com>
2018-07-01 09:06:20 -0400

    Forgot to put this in the right folder

---
+++ mcts/__init__.py
@@ -0,0 +1,18 @@
+import logwood
+from logwood.handlers.stderr import ColoredStderrHandler
+
+# Configure logging
+logwood.basic_config(
+        level = logwood.INFO,
+        handlers = [ColoredStderrHandler()]
+)
+
+SUPPORTED_POLICY_TYPES = {
+    'action' : ['most-visited','proportional-to-visit-count'],
+    'selection': ['ucb1', 'puct'],
+    'expansion' : ['vanilla','neural'],
+    'simulation' : ['to-end'],
+    'update' : ['vanilla', 'value'],
+    'simulation_rollout' : ['random-unvisited','random'],
+    'expansion_rollout': ['random-unvisited','random']
+}
