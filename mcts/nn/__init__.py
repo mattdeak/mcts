ec49225231ede33176f88dc9292d1b2aa7d4e454
Matt <mwoolnerdeakos@gmail.com>
2018-07-01 11:25:57 -0400

    forgot __init__.py in nn module

---
+++ mcts/nn/__init__.py
@@ -0,0 +1,4 @@
+from mcts.nn.utils import crossentropy_with_logits
+from keras.utils.generic_utils import get_custom_objects
+
+get_custom_objects().update({"crossentropy_with_logits" : crossentropy_with_logits})
